8b5caa4307e08cdb1b24f0a554ac93e7018c9eab
Todd Brannon <toddqbrannon@gmail.com>
2023-03-07 13:57:07 -0600

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const workoutRoutes = require('./routes/workouts')
 
 // exppress app
@@ -31,5 +32,20 @@ mongoose.connect(process.env.MONGO_URI)
     })
 
 const port = process.env.PORT
+
+// Enable CORS
+const allowedOrigins = [process.env.ACCESS_CONTROL_ALLOW_ORIGIN];
+app.use(cors({
+  origin: function(origin, callback) {
+    // Allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
